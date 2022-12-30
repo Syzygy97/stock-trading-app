978346d8885c9eb63adc87cdb0aa570498af775e
aimielmarvin <aimielmarvin26@gmail.com>
2022-12-30 22:09:19 +0800

    Generated home model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'homes/index'
   get 'stocks/index'
   get 'stocks/show'
   get 'stocks/new'
@@ -16,5 +17,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homes#index"
 end
