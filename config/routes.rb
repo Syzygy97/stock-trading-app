aa34ebe368630524a1682ac154a40a17259f6746
aimielmarvin <aimielmarvin26@gmail.com>
2022-12-30 21:43:28 +0800

    Generated portfolio and stock controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'stocks/index'
+  get 'stocks/show'
+  get 'stocks/new'
+  get 'stocks/create'
+  get 'stocks/edit'
+  get 'stocks/update'
+  get 'stocks/destroy'
+  get 'portfolios/index'
+  get 'portfolios/show'
+  get 'portfolios/new'
+  get 'portfolios/create'
+  get 'portfolios/edit'
+  get 'portfolios/update'
+  get 'portfolios/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
