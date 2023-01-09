e082aaf75e95a01414fbdef8adf041d0606b74a0
aimielmarvin <aimielmarvin26@gmail.com>
2023-01-09 16:34:57 +0800

    Set initial routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: %i[ show edit update destroy ]
+
   def index
     @portfolios = Portfolio.all
   end
@@ -11,6 +13,13 @@ class PortfoliosController < ApplicationController
   end
 
   def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    if @portfolio.save
+      redirect_to portfolios_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,4 +31,14 @@ class PortfoliosController < ApplicationController
   def destroy
   end
   
+  private
+
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    def portfolio_params
+      params.require(:portfolio).permit(:balance)
+    end
+
 end
