20851a8f14d19a6685307b259338d4ab6d4826fa
aimielmarvin <aimielmarvin26@gmail.com>
2023-01-10 19:57:45 +0800

    Added other columns and update name column of stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_10_092917) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_110209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,11 +54,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_092917) do
   end
 
   create_table "stocks", force: :cascade do |t|
-    t.string "name"
+    t.string "company_name"
     t.integer "price"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "symbol"
+    t.string "logo"
   end
 
   create_table "users", force: :cascade do |t|
