c31c98a177604bc4d08bf138bda481beb43ace52
Kiara Marie <koodlem@gmail.com>
2023-01-25 07:06:07 +0800

    Modified emails

--- app/mailers/approved_account_mailer.rb
+++ app/mailers/approved_account_mailer.rb
@@ -1,6 +1,6 @@
 class ApprovedAccountMailer < ApplicationMailer
   def approve_email
     @email = params[:email]
-    mail(to: @email, subject: '[Approved Account] Welcome to Stock Trading App')
+    mail(to: @email, subject: '[ACCOUNT APPROVED] Welcome to ASTR!')
   end
 end
