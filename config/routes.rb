10b28892b19106479e5df46ff63980c5c15aa0eb
Kiara Marie <koodlem@gmail.com>
2023-01-25 16:48:28 +0800

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
   
   get 'homes/index'
   
@@ -13,36 +13,17 @@ Rails.application.routes.draw do
   resources :orders
   resources :stocks
   # devise_for :users, path: 'users', controllers: {
-    #   sessions: 'users/sessions',
-    #   registrations: 'users/registrations'
-    # }
+  #     sessions: 'users/sessions',
+  #     registrations: 'users/registrations'
+  #   }
 
-  # devise_scope :user do
-  #   authenticated :user do
-  #     namespace :users do
-  #       get 'portfolios/index', as: :authenticated_root
-  #       get 'portfolios', to: "portfolios#index", as: "portfolios"
-  #       get 'portfolios/:id', to: "portfolios#show", as: "portfolio"
-  #       get 'portfolio/new', to: "portfolios#new", as: "new_portfolio"
-  #       post 'portfolios', to: "portfolios#create"
-  #       get 'portfolios/:id/edit', to: "portfolios#edit", as: "edit_portfolio"
-  #       patch 'portfolios/:id', to: "portfolios#update", as: "update_portfolio"
-  #       delete 'portfolios/:id', to: "portfolios#destroy"
-  
-  #       get 'stocks', to: "stocks#index", as: "stocks"
-  #       get 'stocks/:id', to: "stocks#show", as: "stock"
-  #       get 'stock/new', to: "stocks#new", as: "new_stock"
-  #       post 'stocks/create', to: "stocks#create"
-  #       get 'stocks/:id/edit', to: "stocks#edit", as: "edit_stock"
-  #       patch 'stocks/update', to: "stocks#update"
-  #       delete 'stocks/:id', to: "stocks#destroy"
-  
-  #       get 'histories', to: "histories#index", as: "histories"
-  #       get 'histories/:id', to: "histories#show", as: "history"
-  #       delete 'histories/:id', to: "histories#destroy"
-  #     end
-  #   end
-  # end
+  devise_scope :user do
+    authenticated :user do
+      namespace :users do
+        get 'dashboard', to: "users#index", as: :authenticated_root
+      end
+    end
+  end
   
   devise_for :admins, path: 'admins', controllers: {
     sessions: 'admins/sessions',
