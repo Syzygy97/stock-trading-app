68e0dcc6637b126a4852a23387b87468414d29a9
aimielmarvin <aimielmarvin26@gmail.com>
2023-01-25 18:38:57 +0800

    Finished styling of pages without styling

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,7 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy ]
   before_action :set_client, only: %i[ index show edit update destroy ]
+  before_action :authenticate_user!
 
   def index
     @stocks = Stock.all
